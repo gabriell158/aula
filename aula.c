7811350fbccf62470f2c419efe14a7a05827078c
Pampuch <gabripampuch@hotmail.com>
2019-11-01 08:36:04 -0200

    teste

--- aula.c
+++ aula.c
@@ -35,5 +35,6 @@ int main()
 >>>>>>> 38e7a6461c77e2d1dbcf1bc12899dee02713b470
 >>>>>>> e3eda6d2d6789fd40b6ebea0fd4bc4aa61c5ca23
 >>>>>>> 3f61d7d2abe919dd45c2dcde16ac2b924f3c1bc7
+	printf("Gabriel");
 	return 0;
 }
